a3ffd9928f460ff4ac945d1e18e7839f0ec55cc0
Макбук <makbuk@Air-Makbuk.fios-router.home>
2020-10-28 01:02:28 -0400

    add structure: data folder, split tests

---
+++ test/smoke/initial.js
@@ -0,0 +1,65 @@
+const sel = require('../../data/selectors.json')
+const exp = require('../../data/expected.json')
+
+describe('My Little Hero', function () { //define suite title by passing a string
+
+    describe('Getting to the page', function () {
+
+        it('TC-001 Title is correct ', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+            let title = browser.getTitle();
+            expect(title).toEqual('MLH trial');
+        });
+
+    });
+
+    describe('Elements exist', function () {
+
+        it('TC-002 Label for name', function () {
+            const label = $$(sel.label)[0].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-003 Label for gender', function () {
+            const label = $$(sel.label)[1].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-004 Label for age', function () {
+            const label = $$(sel.label)[2].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-005 Label for story', function () {
+            const label = $$(sel.label)[3].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+    });
+
+        describe('Labels are correct', function () {
+
+        it('TC-006 Label for name "1. What is your HERO\'s name?"', function () {
+            const text = $$('.ant-form-item-required')[0].getAttribute('title');
+            expect(text).toEqual(exp.labelName);
+        });
+
+        it('TC-007 Label for gender = 2. Please choose a gender', function () {
+            const label = $$('.ant-form-item-required')[1].getAttribute('title');
+            expect(text).toEqual(exp.labelGender);
+        });
+        //
+        // it('TC-008 Label for age', function () {
+        //     const label = $$('.ant-form-item-required')[2].isDisplayed();
+        //     expect(label).toEqual(true);
+        // });
+        //
+        // it('TC-009 Label for story', function () {
+        //     const label = $$('.ant-form-item-required')[3].isDisplayed();
+        //     expect(label).toEqual(true);
+        // });
+
+    });
+
+});
+
